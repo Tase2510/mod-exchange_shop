5d24b45bd04681087e247a8b1c1b2f1828d92b00
luk3yx <luk3yx@users.noreply.github.com>
2023-02-27 10:01:24 +1300

    Make crafting recipe work on minetest_game (#2)

--- init.lua
+++ init.lua
@@ -1,3 +1,14 @@
+-- Try and find a ruby item
+local ruby
+if minetest.registered_items["default:ruby"] then
+	ruby = "default:ruby"
+elseif minetest.registered_items["default:mese_crystal"] then
+	ruby = "default:mese_crystal"
+else
+	-- Disable exchange shops if they would be uncraftable
+	return
+end
+
 exchange_shop = {}
 exchange_shop.storage_size = 5 * 4
 exchange_shop.shopname = "exchange_shop:shop"
@@ -8,6 +19,16 @@ exchange_shop.S = minetest.get_translator("exchange_shop")
 local modpath = minetest.get_modpath("exchange_shop")
 dofile(modpath .. "/shop_functions.lua")
 dofile(modpath .. "/shop.lua")
+
+
+minetest.register_craft({
+	output = exchange_shop.shopname,
+	recipe = {
+		{"", ruby, ""},
+		{"default:gold_ingot", "default:chest", "default:gold_ingot"},
+		{"", "default:gold_ingot", ""}
+	}
+})
 
 if mesecon and mesecon.register_mvps_stopper then
 	mesecon.register_mvps_stopper(exchange_shop.shopname)
