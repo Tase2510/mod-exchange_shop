2956418342ddd3ff68d004c8c2bb6f30196c7608
Maksym H <Maksym48@pm.me>
2023-07-15 16:09:05 +0300

    Allow loading mod without a valid craft recipe

--- init.lua
+++ init.lua
@@ -1,3 +1,17 @@
+exchange_shop = {
+	S = minetest.get_translator("exchange_shop"),
+	storage_size = 5 * 4,
+	shopname = "exchange_shop:shop"
+}
+
+local modpath = minetest.get_modpath("exchange_shop")
+dofile(modpath .. "/shop_functions.lua")
+dofile(modpath .. "/shop.lua")
+
+if mesecon and mesecon.register_mvps_stopper then
+	mesecon.register_mvps_stopper(exchange_shop.shopname)
+end
+
 -- Try and find a ruby item
 local ruby
 if minetest.registered_items["default:ruby"] then
@@ -5,21 +19,8 @@ if minetest.registered_items["default:ruby"] then
 elseif minetest.registered_items["default:mese_crystal"] then
 	ruby = "default:mese_crystal"
 else
-	-- Disable exchange shops if they would be uncraftable
 	return
 end
-
-exchange_shop = {}
-exchange_shop.storage_size = 5 * 4
-exchange_shop.shopname = "exchange_shop:shop"
-
--- Internationalisation
-exchange_shop.S = minetest.get_translator("exchange_shop")
-
-local modpath = minetest.get_modpath("exchange_shop")
-dofile(modpath .. "/shop_functions.lua")
-dofile(modpath .. "/shop.lua")
-
 
 minetest.register_craft({
 	output = exchange_shop.shopname,
@@ -29,7 +30,3 @@ minetest.register_craft({
 		{"", "default:gold_ingot", ""}
 	}
 })
-
-if mesecon and mesecon.register_mvps_stopper then
-	mesecon.register_mvps_stopper(exchange_shop.shopname)
-end
